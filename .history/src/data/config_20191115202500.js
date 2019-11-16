8336049672b681bafa9383258d9a6502f2e2a86f
gaboesquivel <contact@gaboesquivel.com>
2019-11-15 20:28:21 -0600

    initial commit

---
+++ .history/src/data/config_20191115202500.js
@@ -0,0 +1,31 @@
+module.exports = {
+	defaultTitle: 'The TELOS DreamStack Project',
+	logo: 'https://portfolio.blockmatic.com/favicon/favicon-512.png',
+	author: 'Blockmatiic',
+	url: 'https://portfolio.blockmatic.com',
+	legalName: 'Blockmatiic',
+	defaultDescription: 'Jumpstart dApp development.',
+	socialLinks: {
+		twitter: 'http://www.twitter.com/blockmatic',
+		github: 'https://github.com/blockmatic',
+	},
+	googleAnalyticsID: '',
+	themeColor: '#6b63ff',
+	backgroundColor: '#6b63ff',
+	social: {
+		facebook: 'appId',
+		twitter: '@blockmatic',
+	},
+	address: {
+		city: 'City',
+		region: 'Region',
+		country: 'Country',
+		zipCode: 'ZipCode',
+	},
+	contact: {
+		email: 'email',
+		phone: 'phone number',
+	},
+	foundingDate: '2018',
+	recaptcha_key: '6Lcs6lQUAAAAAEwhNH2IsobIe2csdda4TU3efpMN'
+}
