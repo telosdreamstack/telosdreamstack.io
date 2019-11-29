9cfea3224b9a352bbd0d624debd05d2fb1a661c9
gaboesquivel <contact@gaboesquivel.com>
2019-11-28 20:40:31 -0600

    content: update diagram

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Layout, SEO } from 'components/common'
 import { Intro, Skills, Contact, Projects } from 'components/landing'
-import diagramImg from 'assets/TelosDreamStackFull.png'
+import diagramImg from 'assets/TelosDreamStackFull.svg'
 import { Container } from 'components/common'
 
 export default () => (
@@ -9,7 +9,7 @@ export default () => (
     <SEO />
     <Intro />
     <Container>
-      <img src={diagramImg} alt="" />
+    <img src={diagramImg} alt="" />
     </Container>
     <Projects />
     <Skills />
